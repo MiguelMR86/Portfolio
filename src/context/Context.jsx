2a150eedf3bb9cbc24ffab8c8df1e3dcfb76745c
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-09-23 22:46:25 +0100

    Main > Projects ( Completed )

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -7,12 +7,93 @@ export function ContextProvider({ children, value: {...other} }) {
   const [openMenu, setOpenMenu] = useState(false);
   const [currentSection, setCurrentSection] = useState('home');
 
+  const projects = [
+    {
+        'name': 'TaskHub',
+        'shortDescription': 'TaskHub es una aplicación web que permite a los usuarios gestionar y organizar sus tareas de manera eficiente',
+        'technologies': {
+            'frontend':['HTML', 'tailwindcss', 'JavaScript','React', 'Material-Tailwind'],
+            'backend':['Firebase', 'Node', 'Vite']
+        },
+        'links': {
+            'github': 'https://github.com/MiguelMR86/TaskHub',
+            'web': 'https://taskhub-46037.web.app/',
+            'img': 'https://user-images.githubusercontent.com/90867675/259472945-caf02039-6d04-4551-8d75-f2e62cc5bd82.png'
+        }
+    },
+    {
+        'name': 'METEC',
+        'shortDescription': 'E-commerce of technologies products made by Miguel and Edvin',
+        'technologies': {
+            'frontend':['HTML', 'tailwindcss', 'JavaScript','React', 'Material-Tailwind', 'DaisyUI', 'TsParticles'],
+            'backend':['Firebase', 'Node', 'Vite']
+        },
+        'links': {
+            'github': 'https://github.com/Edvintrabajo/metec-ecommerce',
+            'web': 'https://test-metec-3.web.app/',
+            'img': 'https://user-images.githubusercontent.com/90867675/269961501-1eceb756-e372-4dbd-9917-b3267b7bb0a9.png'
+        }
+    },
+    {
+        'name': 'InfinityGif',
+        'shortDescription': 'InfinityGif es una aplicación web que te permite explorar y descargar gifs de manera infinita a medida que haces scroll',
+        'technologies': {
+            'frontend':['HTML', 'tailwindcss', 'JavaScript','React', 'Material-Tailwind'],
+            'backend':['Node', 'Vite']
+        },
+        'links': {
+            'github': 'https://github.com/MiguelMR86/InfinityGif',
+            'web': 'https://infinity-gif.vercel.app/',
+            'img': 'https://user-images.githubusercontent.com/90867675/259465936-7f572ad0-8c90-41b9-86ba-456dd9294f64.png'
+        }
+    },
+    {
+      'name': 'Planetary',
+      'shortDescription': 'Planetary es un proyecto que combina Three.js y React para crear un emocionante planetario 3D interactivo',
+      'technologies': {
+          'frontend':['HTML', 'tailwindcss', 'JavaScript','React', 'Three.js'],
+          'backend':['Node', 'Vite']
+      },
+      'links': {
+          'github': 'https://github.com/MarioDevv/Planetary',
+          'web': 'https://planetary-orcin.vercel.app/',
+          'img': 'https://user-images.githubusercontent.com/90867675/259512688-929c7cdd-9e1e-4b7d-b2ed-5e7c70b1bda1.png'
+      }
+    },
+    {
+      'name': 'FiftySaving-Vue',
+      'shortDescription': 'Esta es una simple app con una funcionalidad simple, te permite ahorrar 1275 € en 50 días',
+      'technologies': {
+          'frontend':['HTML', 'CSS', 'JavaScript','Vue', 'Pinia'],
+          'backend':['Node', 'Vite']
+      },
+      'links': {
+          'github': 'https://github.com/MiguelMR86/FiftySaving-Vue',
+          'web': 'https://fifty-saving.vercel.app/',
+          'img': 'https://user-images.githubusercontent.com/90867675/267636913-a2ebb9a0-7218-4629-8eef-1379502b2ff0.png'
+      }
+    },
+    {
+      'name': 'Calculator-Vue',
+      'shortDescription': 'Esta es una calculadora web avanzada desarrollada con Vue 3, TypeScript y Vite',
+      'technologies': {
+          'frontend':['HTML', 'CSS', 'JavaScript','Vue'],
+          'backend':['Node', 'Vite']
+      },
+      'links': {
+          'github': 'https://github.com/MiguelMR86/Calculator-Vue',
+          'web': 'https://calculator-vue-ts.netlify.app/',
+          'img': 'https://user-images.githubusercontent.com/90867675/265047903-4fe77e86-7f55-4d9c-a130-36fa0699f61e.png'
+      }
+    },
+]
   // Handlers
   const handleOpenMenu = () => setOpenMenu(!openMenu);
 
   const operators = {
     openMenu, handleOpenMenu,
-    currentSection, setCurrentSection
+    currentSection, setCurrentSection,
+    projects
   };
 
   return (
