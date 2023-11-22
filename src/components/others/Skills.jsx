4172008fe5b70aff123326730d62b169ff60000e
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2023-11-22 21:38:15 +0000

    Skills ( Updated )

--- src/components/others/Skills.jsx
+++ src/components/others/Skills.jsx
@@ -27,27 +27,91 @@ export class skills extends Component {
                     <div className="bg-[--third] w-[300px] h-[400px] rounded-md">
                         <h1 className="text-2xl h-1/6 grid place-items-center">Front-end</h1>
                         <div className="w-full h-5/6 p-4 flex flex-wrap justify-center">
-                            <BiLogoHtml5 size={40} className="w-1/2 text-[#f16529]"/>
-                            <DiCss3 size={40} className="w-1/2 text-[#2465f1]"/>
-                            <TbBrandJavascript size={40}  className="w-1/2 text-[#f7e025]"/>
-                            <BiLogoTypescript size={40} className="w-1/2 text-[#377cc8]"/>
-                            <BiLogoReact size={40} className="w-1/2 text-[#0c9fcb]"/>
-                            <FaVuejs size={40} className="w-1/2 text-[#47ba87]"/>
-                            <BiLogoTailwindCss size={40} className="w-1/2 text-[#38bdf8]"/>
-                            <FaBootstrap size={35} className="w-1/2 text-[#7952b3]"/>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://developer.mozilla.org/es/docs/Web/HTML">
+                                    <BiLogoHtml5 size={40} className=" text-[#f16529]" title="Html"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://developer.mozilla.org/es/docs/Web/CSS">
+                                    <DiCss3 size={40} className=" text-[#2465f1]" title="Css"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://developer.mozilla.org/es/docs/Web/JavaScript">
+                                    <TbBrandJavascript size={40}  className="text-[#f7e025]" title="Javascript"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://www.typescriptlang.org/">
+                                    <BiLogoTypescript size={40} className="text-[#377cc8]" title="Typescript"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://es.reactjs.org/">
+                                    <BiLogoReact size={40} className="text-[#0c9fcb]" title="React"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://vuejs.org/">
+                                    <FaVuejs size={40} className="text-[#47ba87]" title="Vue"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://tailwindcss.com/">
+                                    <BiLogoTailwindCss size={40} className="text-[#38bdf8]" title="Tailwindcss"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://getbootstrap.com/">
+                                    <FaBootstrap size={35} className="text-[#7952b3]" title="Bootstrap"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <div className="bg-[--third] w-[300px] h-[400px] rounded-md">
                         <h1 className="text-2xl h-1/6 grid place-items-center">Back-end</h1>
                         <div className="w-full h-5/6 p-4 flex flex-wrap justify-center">
-                            <FaNodeJs size={40} className="w-1/2 text-[#93c745]"/>
-                            <BiLogoFirebase size={40} className="w-1/2 text-[#ffcd33]"/>
-                            <SiExpress size={40} className="w-1/2"/>
-                            <GrDocker size={40} className="w-1/2 text-[#0db7ed]"/>
-                            <LiaPhp size={40} className="w-1/2 text-[#7b7fb5]"/>
-                            <GrMysql size={40} className="w-1/2 text-[#08668e]"/>
-                            <BiLogoPython size={40} className="w-1/2 text-[#3776ab]"/>
-                            <FaLaravel size={40} className="w-1/2 text-[#ff2d20]"/>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://nodejs.org/es/">
+                                    <FaNodeJs size={40} className="text-[#93c745]" title="NodeJs"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://firebase.google.com/">
+                                    <BiLogoFirebase size={40} className="text-[#ffcd33]" title="Firebase"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://www.php.net/">
+                                    <LiaPhp size={40} className="text-[#7b7fb5]" title="Php"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://laravel.com/">
+                                    <FaLaravel size={40} className="text-[#ff2d20]"  title="Laravel"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://expressjs.com/es/">
+                                    <SiExpress size={40} className=""  title="Laravel"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://www.docker.com/">
+                                    <GrDocker size={40} className="text-[#0db7ed]" title="Docker"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://www.mysql.com/">
+                                    <GrMysql size={40} className="text-[#08668e]" title="Mysql"/>
+                                </a>
+                            </div>
+                            <div className="w-1/2 grid place-items-center">
+                                <a href="https://www.python.org/">
+                                    <BiLogoPython size={40} className="text-[#3776ab]" title="Python"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </article>
