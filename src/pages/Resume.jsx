39107959ada5340ecc1ec1df6eeffb023efe6590
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2024-05-05 12:32:51 +0100

    Resume update

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,13 @@
 import React, { useContext } from "react"
 import { Context } from "../context/Context"
 import SectionTitle from "../components/SectionTitle"
-import SectionDescription from "../components/SectionDescription"
 import Card from "../components/cards/Card"
 import ScrollToButton from "../components/buttons/ScrollToButton"
 import { IoIosArrowUp } from "react-icons/io"
 import { IoCodeSlash } from "react-icons/io5"
 import { TbTournament } from "react-icons/tb"
+import { LuGraduationCap } from "react-icons/lu"
+import { IoBriefcaseOutline } from "react-icons/io5"
 
 const Resume = () => {
   const { scrollToTopSmoothById } = useContext(Context)
@@ -17,15 +18,59 @@ const Resume = () => {
   return (
     <div
       id="about-main"
-      className={`relative w-full bg-primary rounded-xl mt-[100px] md:mt-0 p-6 ${width} ${height}`}
+      className={`relative w-full bg-primary rounded-xl mt-[100px] md:mt-0 p-10 ${width} ${height}`}
     >
-      <SectionTitle title="About" />
-      <div className="overflow-y-auto  h-[calc(100%-10.5%)]">
-        <SectionDescription description="Soy un Desarrollador Full Stack al que le encanta transformar ideas creativas en código sólido. Como desarrollador, busco crear soluciones escalables que mejoren la experiencia del usuario." />
+      <SectionTitle title="Trayectoria" />
 
-        <div className="mt-6">
-          <h2 className="font-semibold text-2xl mb-4">What I do?</h2>
-          <div className="flex flex-col gap-4 pb-16 xl:flex-row">
+      <div className="flex gap-8 mt-6 overflow-y-auto h-[calc(100%-68px)]">
+        <div>
+          <div className="flex items-center gap-2 mb-4">
+            <LuGraduationCap size={30} color="#0b73e1" />
+            <h2 className="font-semibold text-2xl">Educación</h2>
+          </div>
+          <div className="flex flex-col gap-4 pb-16">
+            <Card
+              title="Full-Stack"
+              className={"bg-sky-100/60"}
+              icon={<IoCodeSlash size={35} color="#7F4AA6" />}
+              children={
+                <div>
+                  <p className="mb-2">
+                    <span className="font-semibold mr-1">Frotnend:</span>
+                    Desarrollo de aplicaciones web y móviles, con interfaces
+                    atractivas e intuitivas.
+                  </p>
+
+                  <p>
+                    <span className="font-semibold mr-1">Backend:</span>
+                    Desarrollo de servicios web, APIs y bases de datos.
+                  </p>
+                </div>
+              }
+            />
+
+            <Card
+              title="SEO"
+              className={"bg-sky-100/60"}
+              icon={<TbTournament size={30} color="#e2c060" />}
+              children={
+                <div>
+                  <p className="mb-2">
+                    Optimización de motores de búsqueda para mejorar la
+                    visibilidad de los sitios web.
+                  </p>
+                </div>
+              }
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center gap-2 mb-4">
+            <IoBriefcaseOutline size={25} color="#0b73e1" />
+            <h2 className="font-semibold text-2xl">Experiencia</h2>
+          </div>
+          <div className="flex flex-col gap-4 pb-16 ">
             <Card
               title="Full-Stack"
               className={"bg-sky-100/60"}
@@ -65,7 +110,7 @@ const Resume = () => {
 
       <ScrollToButton
         className="absolute bottom-[1.4em] right-5 p-3 bg-white rounded-lg shadow-default/30 shadow-md md:hidden"
-        Icon={() => <IoIosArrowUp size={24} />}
+        Icon={<IoIosArrowUp size={24} />}
         onClick={() => scrollToTopSmoothById("about-header")}
       />
     </div>
