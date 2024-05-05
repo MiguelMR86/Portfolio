bf7bcec6f9eda81de3443046da27d78820a7be25
Miguel Ángel Medina Ramírez <migueldevinfo@gmail.com>
2024-05-05 11:43:51 +0100

    Menu fixes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,25 +1,13 @@
-import { useContext } from "react"
-import { Context } from "../context/Context"
-import menuSections from "./sections/menuSections"
-import Button from "./buttons/Button"
+import MenuSections from "./sections/MenuSections"
 import PropTypes from "prop-types"
 
 function Menu(props) {
-  const { setCurrentSection } = useContext(Context)
   const { className } = props
   return (
     <div
       className={`${className} bg-primary flex gap-4 justify-between px-4 items-center rounded-xl backdrop-blur-md text-white shadow-default`}
     >
-      {menuSections.map((item, index) => (
-        <Button
-          key={index}
-          Icon={item.Icon}
-          section={item.section}
-          onClick={() => setCurrentSection(item.section)}
-          className="w-[60px] h-[60px]  rounded-lg"
-        />
-      ))}
+      <MenuSections />
     </div>
   )
 }
--- src/components/sections/MenuSections.jsx
+++ src/components/sections/MenuSections.jsx
@@ -1,29 +1,46 @@
+import { useContext } from "react"
+import { Context } from "../../context/Context"
+import Button from "../buttons/Button"
 import { BsPerson } from "react-icons/bs"
 import { AiOutlineFolderOpen, AiOutlineMessage } from "react-icons/ai"
 import { GrDocumentText } from "react-icons/gr"
 import { IoBriefcaseOutline } from "react-icons/io5"
 
-const menuSections = [
-  {
-    Icon: BsPerson,
-    section: "About",
-  },
-  {
-    Icon: GrDocumentText,
-    section: "Resume",
-  },
-  // {
-  //   Icon: AiOutlineFolderOpen,
-  //   section: "Projects",
-  // },
-  {
-    Icon: IoBriefcaseOutline,
-    section: "Work",
-  },
-  {
-    Icon: AiOutlineMessage,
-    section: "Contact",
-  },
-]
+import React from "react"
+const { setCurrentSection } = useContext(Context)
 
-export default menuSections
+const MenuSections = () => {
+  const menuSections = [
+    {
+      Icon: BsPerson,
+      section: "About",
+    },
+    {
+      Icon: GrDocumentText,
+      section: "Resume",
+    },
+    // {
+    //   Icon: AiOutlineFolderOpen,
+    //   section: "Projects",
+    // },
+    {
+      Icon: IoBriefcaseOutline,
+      section: "Work",
+    },
+    {
+      Icon: AiOutlineMessage,
+      section: "Contact",
+    },
+  ]
+  return menuSections.map((item, index) => (
+    <Button
+      key={index}
+      Icon={item.Icon}
+      section={item.section}
+      onClick={() => setCurrentSection(item.section)}
+      className="w-[60px] h-[60px]  rounded-lg"
+    />
+  ))
+}
+
+export default MenuSections
